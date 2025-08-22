9fb70111a8f8e02def56332038285552c80325b8
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-08-22 17:24:15 +0530

    {   params: Promise<{ model: string }>; }

--- src/app/dashboard/[model]/page.tsx
+++ src/app/dashboard/[model]/page.tsx
@@ -1,12 +1,13 @@
 import CurrentStock from "@/components/Stock Dashboard/CurrentStock";
 
-const SpecificModelPage = async ({ params }: { params: { model: string } }) => {
-  async function getParams() {
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    const { model } = await params;
-    return model;
-  }
-  const model = await getParams();
+const SpecificModelPage = async ({
+  params,
+}: {
+  params: Promise<{ model: string }>;
+}) => {
+  // Properly await the params promise
+  const { model } = await params;
+
   return (
     <div className='container mx-auto px-4 py-8'>
       <CurrentStock model={model} />
