c4b3de7b7013e67f1f66d41052a2e92a55883487
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-08-22 17:20:24 +0530

    created an async function to await params

--- src/app/dashboard/[model]/page.tsx
+++ src/app/dashboard/[model]/page.tsx
@@ -1,11 +1,12 @@
 import CurrentStock from "@/components/Stock Dashboard/CurrentStock";
 
 const SpecificModelPage = async ({ params }: { params: { model: string } }) => {
-  // async function getParams() {
-  //   return model;
-  // }
-  // const model = await getParams();
-  const { model } = await params;
+  async function getParams() {
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const { model } = params;
+    return model;
+  }
+  const model = await getParams();
   return (
     <div className='container mx-auto px-4 py-8'>
       <CurrentStock model={model} />
